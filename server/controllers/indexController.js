8419420bd0c72aa66a0e0e0ab4d51f13b222fcfc
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-07-27 19:32:58 -0400

    chore: clean old files

--- server/controllers/indexController.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-
-export async function showHomePage(req, res){
-    console.log('someone hit the thing')
-}
--- server/routes/indexRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as indexController from '../controllers/indexController.js';
-
-const indexRouter = Router();
-
-indexRouter.get('/', indexController.showHomePage);
-
-export { indexRouter };
